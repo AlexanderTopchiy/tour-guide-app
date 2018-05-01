a36e260c65fcb3466f20413f467b2325e06ed40f
Alexander Topchiy <topchiyau@gmail.com>
2018-05-01 15:12:59 +0300

    Add data in Culture category

--- app/src/main/java/com/example/android/tourguide/CultureFragment.java
+++ app/src/main/java/com/example/android/tourguide/CultureFragment.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
  */
 public class CultureFragment extends Fragment {
 
-
     public CultureFragment() {
         // Required empty public constructor
     }
@@ -25,20 +24,33 @@ public class CultureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.places_list, container, false);
-
         // Create an array of places
         ArrayList<Place> places = new ArrayList<Place>();
 
+        // Add to an array new objects with the data
         places.add(new Place("Musical theater", R.drawable.musical_theater));
         places.add(new Place("Organ Hall", R.drawable.organ_hall));
+        places.add(new Place("Philharmonic Hall", R.drawable.philharmonic_hall));
+        places.add(new Place("Central Concert Hall", R.drawable.central_concert_hall));
+        places.add(new Place("Puppet theater", R.drawable.puppet_theater));
+        places.add(new Place("Central Exhibition Hall", R.drawable.central_exhibition_hall));
+        places.add(new Place("Regional Art Museum", R.drawable.regional_art_museum));
+        places.add(new Place("State Historical and Archaeological Museum-Reserve",
+                             R.drawable.state_historical_and_archaeological_museum_reserve));
+        places.add(new Place("Pushkin Regional Universal Scientific Library",
+                              R.drawable.pushkin_regional_universal_scientific_library));
 
+        // Inflate current View object in places_list.xml
+        View rootView = inflater.inflate(R.layout.places_list, container, false);
+
+        // Create PlaceAdapter object in current activity for data from places array
         PlaceAdapter placeAdapter = new PlaceAdapter(getActivity(), places);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.list);
-
+        // Create ListView object in places_list.xml and set the PlaceAdapter object to it
+        ListView listView = rootView.findViewById(R.id.list);
         listView.setAdapter(placeAdapter);
 
+        // Return current View object with the data
         return rootView;
     }
 
--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
         CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
